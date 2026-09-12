8ce5e33d849dabaaa0992ab673c0427738e0ee79
kanchanchoudhary02 <kanchan31choudhary@gmail.com>
2026-09-12 14:47:55 +0530

    emailrequestfixing

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -12,6 +12,7 @@ function getTransporter() {
 
   transporter = nodemailer.createTransport({
     service: "gmail",
+    family: 4,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
